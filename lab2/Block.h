38daade064b21c6fbd8a2253f792514200f42c3e
rey9990@gmail.com <rey9990@gmail.com@0565a22f-31c0-1274-0085-c4a3781606b9>
2011-02-02 22:59:40 +0000

    second import

---
+++ lab2/Block.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   Block.h
+ * Author: james
+ *
+ * Created on February 2, 2011, 4:35 PM
+ */
+
+#ifndef BLOCK_H
+#define	BLOCK_H
+
+
+
+#endif	/* BLOCK_H */
+
---
+++ lab2/main.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File:   main.cpp
+ * Author: James Ekstrom
+ * CS 5631 Lab 2
+ * Created on February 1, 2011, 2:35 PM
+ */
+
+#include <cstdlib>
+
+#include "Archive/Disk.h"
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    //test
+    Disk disk1("disk1",100,Disk::DEFAULT_BLOCK_SIZE);
+    disk1.Test();
+    
+    return 0;
+}
+
