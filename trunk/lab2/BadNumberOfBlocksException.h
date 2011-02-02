456a53a6e3753b0d552074bf6a1e840499b59827
rey9990@gmail.com <rey9990@gmail.com@0565a22f-31c0-1274-0085-c4a3781606b9>
2011-02-02 22:59:40 +0000

    second import

---
+++ trunk/lab2/BadNumberOfBlocksException.h
@@ -0,0 +1,18 @@
+/* 
+ * File:   BadNumberOfNodesException.h
+ * Author: chris
+ *
+ * Created on January 25, 2009, 1:31 PM
+ */
+
+#ifndef _BADNUMBEROFBLOCKSSEXCEPTION_H
+#define	_BADNUMBEROFBLOCKSSEXCEPTION_H
+
+
+class BadNumberOfBlocksException
+{
+public:
+};
+
+#endif	/* _BADNUMBEROFBLOCKSSEXCEPTION_H */
+
---
+++ trunk/lab2/BlockLinkedList.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   BlockLinkedList.h
+ * Author: james
+ *
+ * Created on February 2, 2011, 4:35 PM
+ */
+
+#ifndef BLOCKLINKEDLIST_H
+#define	BLOCKLINKEDLIST_H
+
+
+
+#endif	/* BLOCKLINKEDLIST_H */
+
