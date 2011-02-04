fb38169f0f5fc94ddc3191c84fb1ed787d8ce24e
rey9990 <rey9990@0565a22f-31c0-1274-0085-c4a3781606b9>
2011-02-04 23:15:35 +0000

    Directory Changes

--- FileSystem/Block.h
+++ FileSystem/Block.h
@@ -14,11 +14,26 @@ private:
     int blockNumber;
     int length;
     char* m_buffer;
+    int next;
 public:
     Block(int);
     Block(int,int);
     Block(int,Disk*);
     ~Block();
+
+    void clearBuffer();
+    void setBuffer(unsigned char*);
+    unsigned char* getBuffer();
+
+    void setBlockNumber(int);
+    int getBlockNumber();
+
+    void setNext(int,int);
+    int getNext(int);
+    
+    void print();
+    bool write(Disk*);
+
 };
 
 
