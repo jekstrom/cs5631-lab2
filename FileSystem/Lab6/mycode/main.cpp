1fc8a5adb02595b86bbb6a2d8e8be45668e421e7
kalebjj@hotmail.com <kalebjj@hotmail.com@0565a22f-31c0-1274-0085-c4a3781606b9>
2011-04-17 03:51:38 +0000

    Phase 3 is looking good.

--- FileSystem/Lab6/mycode/main.cpp
+++ FileSystem/Lab6/mycode/main.cpp
@@ -2,9 +2,21 @@
 #include "message/Message.h"
 #include <iostream>
 #include "headerFiles.h"
+#include <pthread.h>
 
 using namespace muscle;
 using namespace std;
+
+void* connectionThread(void* dataPtr);
+pthread_mutex_t cond_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t  threadsFree = PTHREAD_COND_INITIALIZER;
+bool* threadDone;
+
+struct threadParameter
+{
+    int socID;
+    int threadNumber;
+};
 
 int main(int argc, char** argv) {
 CompleteSetupSystem css; // first line of main
@@ -58,6 +70,11 @@ CompleteSetupSystem css; // first line of main
         // create IPv4 TCP/IP socket
         int sid = socket(AF_INET, SOCK_STREAM, 0);
 
+        if(-1 == sid)
+            cout << "Error: could not create socket. errno = " << errno << endl;
+        else
+            cout << "Socket created successfully." << endl;
+
         // bind socket to given port
         struct addrinfo hints;
         hints.ai_family = AF_INET;
@@ -75,6 +92,7 @@ CompleteSetupSystem css; // first line of main
         if(-1 == bind(sid, result->ai_addr, result->ai_addrlen))
         {
             cout << "Error: failed in binding socket. errno = " << errno << endl;
+            close(sid);
             return EXIT_FAILURE;
         }
         else
@@ -92,50 +110,85 @@ CompleteSetupSystem css; // first line of main
         if(-1 == listen(sid, maxConnections))
         {
             cout << "Error: listening on socket failed. errno = " << errno << endl;
+            close(sid);
             return EXIT_FAILURE;
         }
         else
-            cout << ".\nServer listening for connections." << endl;
+            cout << ".\nServer listening for up to " << maxConnections << " connections." << endl;
 
-        // accept connection request
-        struct sockaddr_in* addrPtr;
-        socklen_t len = (socklen_t) sizeof(struct sockaddr_in);
-        int clientSid = accept(sid, (struct sockaddr *) addrPtr, &len);
-
-        if(0 > clientSid)
-        {
-            cout << "Error: failed to accept connection request. errno = " << errno << endl;
-            return EXIT_FAILURE;
-        }
-        else
-            cout << "Connection request accepted.\n" << endl;
-
-        // *spawn client handler thread here*
-
-        // handler loop for testing
-        int length = 64;
-        char buffer[length];
-        char* bPtr = buffer;
-        string msgStr = "Message acknowledged.";
-        const char* msg = msgStr.c_str();
+        // connection acceptance loop
+        struct sockaddr_in clientAddr;
+        int maxThreads = maxConnections;
+        pthread_t* threads = new pthread_t[maxThreads];
+        threadParameter tParam[maxThreads];
+        threadDone = new bool[maxThreads];
+        for(int i = 0; i < maxThreads; i++)
+            threadDone[i] = true;
+        int nextThread = 0;
+        int freeThreads = maxThreads;
         while(true)
         {
-            if(0 > recv(clientSid, bPtr, length, 0))
-                cout << "Error: failed in receiving from client. errno = " << errno << endl;
+            if(freeThreads > 0)
+            {                
+                socklen_t len = (socklen_t) sizeof(struct sockaddr_in);                
+                tParam[nextThread].socID = accept(sid, (struct sockaddr *) &clientAddr, &len);
+                if(tParam[nextThread].socID == -1)
+                {
+                    cout << "Error: failed to connect with a client.\n";
+                    continue;
+                }
+                else                
+                    cout << "Client number " << (nextThread+1) << " connected." << endl;
+                tParam[nextThread].threadNumber = nextThread;
+
+                // create handler thread
+                int rtn_status = pthread_create(&threads[nextThread], 0, connectionThread, (void*) &tParam[nextThread]);
+                if (rtn_status)
+                {
+                    cerr << "pthread creation failed: " << strerror(rtn_status) << endl;
+                    exit(-1);
+                }
+
+                threadDone[nextThread] = false;
+                freeThreads--;
+                for(int i = 0; i < maxThreads; i++)
+                    if(threadDone[i])
+                    {
+                        nextThread = i;
+                        break;
+                    }
+            }
             else
             {
-                // check for quit command
-                if(string(bPtr) == "quit")
-                    return EXIT_SUCCESS;
+                // join any threads that have completed
+                for(int i = 0; i < maxThreads; i++)
+                    if(threadDone[i])
+                    {
+                        pthread_join(threads[i], NULL);
+                        freeThreads++;
+                        nextThread = i;
+                    }
 
-                cout << "Received message: " << bPtr << endl;
+                // return to accepting if any threads were joined
+                if(freeThreads > 0)
+                    continue;
 
-                if(0 > send(clientSid, msg, msgStr.length(), 0))
-                    cout << "Error: failed in sending to client. errno = " << errno << endl;
-                else
-                    cout << "Sent acknowledgement of message.\n" << endl ;
+                // wait for a client to disconnect
+                cout << "Maximum number of connections reached. ";
+                cout << "Waiting for a client to disconnect." << endl;
+                pthread_cond_wait(&threadsFree, &cond_mutex);
+                for(int i = 0; i < maxThreads; i++)
+                    if(threadDone[i])
+                    {
+                        pthread_join(threads[i], NULL);
+                        freeThreads++;
+                        nextThread = i;
+                    }
+//                cout << "Waiting on client number " << (nextThread+1) << ".\n";
+//                pthread_join(threads[nextThread], NULL);
+//                cout << "Client number " << (nextThread+1) << " disconnected.\n";
             }
-        }
+        } // end while        
     }
     else
     {
@@ -167,6 +220,11 @@ CompleteSetupSystem css; // first line of main
             // create IPv4 TCP/IP socket
             int sid = socket(AF_INET, SOCK_STREAM, 0);
 
+            if(-1 == sid)
+                cout << "Error: could not create socket. errno = " << errno << endl;
+            else
+                cout << "Socket created successfully." << endl;
+
             // connect to server
             struct addrinfo hints;
             hints.ai_family = AF_UNSPEC;
@@ -187,6 +245,7 @@ CompleteSetupSystem css; // first line of main
             if(-1 == connect(sid, result->ai_addr, result->ai_addrlen))
             {
                 cout << "Error: could not connect to server. errno = " << errno << endl;
+                close(sid);
                 return EXIT_FAILURE;
             }
             else
@@ -214,7 +273,10 @@ CompleteSetupSystem css; // first line of main
                     continue;
                 }
                 else if(string(msg) == "quit")
+                {
+                    close(sid);
                     return EXIT_SUCCESS;
+                }
                 else
                     cout << "Message sent, waiting for acknowledgement... ";
 
@@ -226,5 +288,41 @@ CompleteSetupSystem css; // first line of main
 
                 
             }
+    }
+}
+
+void* connectionThread(void* dataPtr)
+{
+    threadParameter* param = (threadParameter*) dataPtr;
+
+    // test thread code
+    int length = 64;
+    char buffer[length];
+    char* bPtr = buffer;
+    string msgStr = "Message acknowledged.";
+    const char* msg = msgStr.c_str();
+    while(true)
+    {
+        if(0 > recv(param->socID, bPtr, length, 0))
+            cout << "Error: failed in receiving from client. errno = " << errno << endl;
+        else
+        {
+            cout << "Received message: " << bPtr << endl;
+
+            // check for quit command
+            if(string(bPtr) == "quit")
+            {
+                close(param->socID);
+                threadDone[param->threadNumber] = true;
+                cout << "Client number " << param->threadNumber+1 << " disconnected." << endl;
+                pthread_cond_signal(&threadsFree);
+                pthread_exit(NULL);
+            }
+
+            if(0 > send(param->socID, msg, msgStr.length(), 0))
+                cout << "Error: failed in sending to client. errno = " << errno << endl;
+            else
+                cout << "Sent acknowledgement of message.\n" << endl ;
+        }
     }
 }
